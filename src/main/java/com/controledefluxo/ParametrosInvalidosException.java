ca0b35b13de2cc551e237b148f0e75439afdb1f7
gdssvpp <pablonogueira16@hotmail.com>
2024-07-02 11:25:45 -0300

    first commit

---
+++ src/main/java/com/controledefluxo/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package com.controledefluxo;
+
+public class ParametrosInvalidosException extends Exception{
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
